f2f4392984b7f1a533489871ee0b7990dfb6f048
KhalidAlsehely <alsehely002@gannon.edu>
2025-05-08 20:57:49 -0400

    Initial commit2

--- JS/scripts.js
+++ JS/scripts.js
@@ -99,7 +99,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
   
         try {
-          const response = await fetch("http://localhost:5000/auth/login", {
+          const response = await fetch("https://vulnerabilitysimulaterapi.aliarthur.com", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
