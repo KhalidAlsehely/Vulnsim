0477327bacf0ed012296820be752b622cef41f05
KhalidAlsehely <alsehely002@gannon.edu>
2025-05-09 22:27:08 -0400

    CORS gotta work pleaes

--- JS/scripts.js
+++ JS/scripts.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
      
-    fetch("https://vulnerabilitysimulaterapi.aliarthur.com/api/auth/login")
+    fetch("https://vulnerabilitysimulaterapi.aliarthur.com/api")
   .then(res => res.json())
   .then(data => console.log(data))
   .catch(err => console.error("Fetch failed:", err));
@@ -104,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
   
         try {
-          const response = await fetch("https://vulnerabilitysimulaterapi.aliarthur.com/api/auth/login", {
+          const response = await fetch("https://vulnerabilitysimulaterapi.aliarthur.com/api", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
